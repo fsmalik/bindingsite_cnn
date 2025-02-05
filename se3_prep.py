8770947832adf2beff5c3df2df93b53841d07329
Faisal Malik <59219252+fsmalik@users.noreply.github.com>
2025-02-05 14:08:27 -0500

    Update se3_prep.py

--- se3_prep.py
+++ se3_prep.py
@@ -1,5 +1,8 @@
 from pdb_pandas import *
 from pathlib import Path
+import itertools
+import dgl
+
 elements_hash = {'H': 1, 'C': 2, 'O': 3, 'N': 4, 'P': 5, 'S': 6}
 
 directory = Path("/home/faisal/tmp/bindingdb_cnn/rcsb_small_testset/combos")
@@ -57,3 +60,40 @@ alldf_ligand = [
     ProcessedPDB(obj.filepath, bonds_ligand_df(obj))
     for obj in process_pdb_multi(files_ligand)
 ]
+
+def dgl_graph(obj_list):
+    """
+    Converts the data from dataframes to DGL Graphs.
+
+    Args:
+        obj_list (list): A list of objects, where each object contains:
+            - `df` (pandas.DataFrame): A DataFrame containing PDB data, including columns like 'bond', 'bond_order', 
+              'hashing', and 'coords' (orthogonal coordinates).
+            - `filepath` (str): The file path to the original PDB file (though not used in graph creation here, included for context).
+
+    Returns:
+        graph_list (list): A list of DGL graphs created from the DataFrames, where each graph contains:
+            - `g` (dgl.graph): A graph object with nodes and edges, where:
+                - Nodes include 'hashing' (node feature) and 'coords' (coordinates of atoms).
+                - Edges include 'bond_order' (bond strength/order between atoms).
+    """
+    graph_list = []
+    for obj in obj_list:
+        df = obj.df
+        all_bonds = list(itertools.chain(*df['bond']))
+        all_bonds_transposed = np.transpose(all_bonds)
+
+        g = dgl.graph((all_bonds_transposed[0], all_bonds_transposed[1]))
+
+        hashing = torch.tensor(df['hashing'].to_numpy()).unsqueeze(1).float()
+        if g.num_nodes() != len(hashing): # For ions
+            g.add_nodes(len(hashing)-g.num_nodes())
+        g.ndata['hashing'] = hashing
+        g.ndata['coords'] = torch.tensor(df[['orth_x', 'orth_y', 'orth_z']].to_numpy()).float()
+        g.edata['bond_order'] = torch.tensor(list(itertools.chain(*df['bond_order']))).float()
+        graph_list.append(g)
+    return graph_list
+    
+
+allg_bs = dgl_graph(alldf_binding_site)
+allg_lig = dgl_graph(alldf_ligand)
